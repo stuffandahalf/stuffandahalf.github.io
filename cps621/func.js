7bef1c1a33442d8aa46f6245ef743a1ba8217f11
Stuff5 <freezedown@hotmail.com>
2018-04-08 03:10:44 -0400

    Made dynamic backgrounds

--- cps621/func.js
+++ cps621/func.js
@@ -5,3 +5,16 @@ function fill() {
     }
     document.getElementById("test").innerHTML = str;
 }
+
+function changeBackground() {
+    var body = document.body;
+    console.log(body.style.backgroundImage);
+    if(body.style.backgroundImage === ""
+        || body.style.backgroundImage === 'url("src/oscar/oscar.jpg")') {
+        document.body.style.backgroundImage = "url('src/tank/tank_crop.jpg')";
+    }
+    else {
+        document.body.style.backgroundImage = "url('src/oscar/oscar.jpg')";
+    }
+    //document.body.style.backgroundImage = "url('src/oscar/oscar.jpg')";
+}
