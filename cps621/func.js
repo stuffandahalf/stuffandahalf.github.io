8d70ddd394acf25afdb9c179ccb9a5bb32154f6c
Stuff5 <freezedown@hotmail.com>
2018-04-08 19:15:54 -0400

    switched to animate.css and jQuery

--- cps621/func.js
+++ cps621/func.js
@@ -1,4 +1,17 @@
-function fill() {
+$(document).ready(function() {
+    $("#begin").click(function() {
+        $("#begin").css({"display": "none"});
+        animate();
+    });
+});
+
+function animate() {
+    console.log("animate");
+    title();
+    frame1();
+}
+
+/*function fill() {
     var str = ""
     for(i = 0; i < 100; i++) {
         str += "<br>";
@@ -17,4 +30,16 @@ function changeBackground() {
         document.body.style.backgroundImage = "url('src/oscar/oscar.jpg')";
     }
     //document.body.style.backgroundImage = "url('src/oscar/oscar.jpg')";
+}*/
+
+function title() {
+    $("#intro").addClass("animated");
+    $("#intro").css("animation-duration: 5s");
+    $("#intro").addClass("bounceIn");
+    $("#intro").css("animation-duration: 3s");
+    $("#intro").addClass("bounceOut");
+}
+
+function frame1() {
+    $("#scene1").addClass("animated bounceIn");
 }
