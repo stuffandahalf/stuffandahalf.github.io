a73f3607149c82b4642ec5173c6b95d02d4d1925
Stuff5 <freezedown@hotmail.com>
2018-04-08 20:10:05 -0400

    switched back to css3 but still using jquery

--- cps621/func.js
+++ cps621/func.js
@@ -11,6 +11,21 @@ function animate() {
     frame1();
 }
 
+function title() {
+    var div = $("#intro");
+    var title = $("#title");
+    div.css({"display": "block"});
+    title.css({"animation-play-state": "running"});
+    title.on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(e) {
+        console.log("finished animation");
+        div.css({"display": "none"});
+    });
+    //delay += parseInt($("#title").css("animation-duration").slice(0, -1));
+    //console.log(delay);
+    
+    
+}
+
 /*function fill() {
     var str = ""
     for(i = 0; i < 100; i++) {
@@ -32,14 +47,47 @@ function changeBackground() {
     //document.body.style.backgroundImage = "url('src/oscar/oscar.jpg')";
 }*/
 
-function title() {
+//function title() {
+function tmp() {
     $("#intro").addClass("animated");
-    $("#intro").css("animation-duration: 5s");
+    
+    /*$("#intro").css("animation-duration: 5s");
     $("#intro").addClass("bounceIn");
     $("#intro").css("animation-duration: 3s");
-    $("#intro").addClass("bounceOut");
+    $("#intro").addClass("bounceOut");*/
+    $("#intro").animateCss("bounceIn", function() {
+        $("#intro").animateCss("bounceOut");
+    });
 }
 
 function frame1() {
-    $("#scene1").addClass("animated bounceIn");
+    console.log("hello");
+    //$("#scene1").addClass("animated bounceIn");
 }
+
+/*$.fn.extend({
+    animateCss: function(animationName, callback) {
+        var animationEnd = (function(el) {
+            var animations = {
+                animation: 'animationend',
+                OAnimation: 'oAnimationEnd',
+                MozAnimation: 'mozAnimationEnd',
+                WebkitAnimation: 'webkitAnimationEnd',
+            };
+
+            for (var t in animations) {
+                if (el.style[t] !== undefined) {
+                    return animations[t];
+                }
+            }
+        })(document.createElement('div'));
+
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+
+            if (typeof callback === 'function') callback();
+        });
+
+        return this;
+    },
+});*/
