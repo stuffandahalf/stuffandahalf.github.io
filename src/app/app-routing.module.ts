a3c319172a33f0e102a4c8255f179f2f4324fadc
Gregory Norton <gregory.norton@me.com>
2021-06-06 03:35:33 -0400

    Added bootstrap and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { BlogComponent } from './blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	//{ path: '', component: HomeComponent },
+	{ path: 'projects', component: ProjectsComponent },
+	{ path: 'experience', component: ExperienceComponent },
+	{ path: 'blog', component: BlogComponent },
+	{ path: '**', component: HomeComponent }
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
