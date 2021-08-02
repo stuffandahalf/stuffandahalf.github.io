fc9348b1d0c8743089e99d7c564e769736d61196
Gregory Norton <gregory.norton@me.com>
2021-08-02 02:33:53 -0400

    Moved static resources into /static

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,15 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		],
+		css: [
+			/*'~/fomantic-ui/dist/semantic.css'*/
+		],
+		script: [
+			/*{ src: '~/fomantic-ui/dist/semantic.js' }*/
 		]
 	}
 };
