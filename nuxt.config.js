e296d9d119909dab4464f5885e7aa02e6b45acb1
Gregory Norton <gregory.norton@me.com>
2021-08-02 01:28:44 -0400

    Started work on vue-based site

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+export default {
+	target: 'static',
+	ssr: false,
+	head: {
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
+		]
+	}
+};
+
